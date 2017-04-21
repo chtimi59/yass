7ce718dd50f887d2e31d3cad3ee9ab0041787b58
jdorgeville <jdorgeville@spiria.com>
2017-04-21 18:17:38 -0400

    drop db if exists

--- setup.sql
+++ setup.sql
@@ -1,6 +1,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+DROP TABLE IF EXISTS `assets`;
 CREATE TABLE IF NOT EXISTS `assets` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -12,6 +13,7 @@ CREATE TABLE IF NOT EXISTS `assets` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `displays`;
 CREATE TABLE IF NOT EXISTS `displays` (
   `id` varchar(255) NOT NULL,
   `ip` varchar(255) DEFAULT NULL,
